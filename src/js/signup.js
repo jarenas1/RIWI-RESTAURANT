3557ffe6b3947743efb85f118d766de84a4e68dc
Juan José Arenas G. <juanjoarenas1218@gmail.com>
2024-08-10 09:23:58 -0500

    check-emaul-function

--- src/js/signup.js
+++ src/js/signup.js
@@ -1,2 +1,29 @@
 import "../scss/signup.scss"
 import * as bootstrap from "bootstrap";
+
+//All ids and classes calleds
+
+const form = document.querySelector("form");
+const name = document.querySelector("#name");
+const email = document.querySelector("#email");
+const emailSpan = document.querySelector("#email-span");
+const password = document.querySelector("#password");
+const passwordSpan = document.querySelector("#password-span");
+const confirmPassword = document.querySelect("#confirm");
+const confirmSpan = document.querySelector("#confirm-span");
+
+//event
+
+
+//Functions
+
+//check if the email is already registered
+async function checkEmail(email) {
+    const response = await fetch(`http://localhost:3000/users`);
+    const data = await response.json(); //pass the response from json to jsObject
+    if (data.length === 0) {
+        return true;
+    }else{
+        return false;
+    }
+}
